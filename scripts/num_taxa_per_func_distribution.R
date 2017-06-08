4d34f2a3b5a9c1b293e0db1c7c1790681a81af47
Gavin Douglas <gavin@kronos.pharmacology.dal.ca>
2017-06-08 14:25:34 -0300

    Added comments to num_taxa_per_func_distribution.R

--- scripts/num_taxa_per_func_distribution.R
+++ scripts/num_taxa_per_func_distribution.R
@@ -1,14 +1,17 @@
 library(gplots)
 
+# Load in accessory functions from other Rscript.
 source("scripts/plot_mean_num_taxa_R_functions.R")
 
+# Load in input files and get mean and standard error for each functional type.
 species_input <- num_taxa_barplot2_input("tables/species_function_counts.txt")
-
 superkingdom_input <- num_taxa_barplot2_input("tables/superkingdom_function_counts.txt")
 
+# Specify 
 cat_names <- c("UniRef100", "UniRef90", "UniRef50", "KEGG\nOrthologs",
                "KEGG\nModules", "KEGG\nPathways")
 
+# Plot barplots and write to pdf as two panels.
 pdf("figures/num_taxa_per_func_distribution.pdf", width = 11, height = 8.5)
 par(mfrow=c(1,2))
 
@@ -21,25 +24,5 @@ barplot2(height = superkingdom_input$means, names.arg = cat_names, plot.ci = TRU
          ci.l = superkingdom_input$lower_ci, ci.u = superkingdom_input$upper_ci,
          ylim=c(0,3), las=3,
          ylab="Mean number of superkingdoms that possess function")
-
-# kingdom_input <- num_taxa_barplot2_input("kingdom_function_counts.txt")
-# barplot2(height = kingdom_input$means, names.arg = cat_names, plot.ci = TRUE,
-#          ci.l = kingdom_input$lower_ci, ci.u = kingdom_input$upper_ci,
-#          ylim=c(0,2), las=3,
-#          ylab="Mean number of kingdoms that possess function")
-
-
-### Converting back to distribution to double-check calculations:
-# tmp <- data.frame(t(read.table("species_function_counts.txt", header=T ,sep="\t",
-#                                row.names=1)))
-# tmp <- tmp[-1,]
-# tmp <- tmp[-nrow(tmp),]
-# xlab_val <- as.numeric(sub("^X", "", rownames(tmp)))
-# ko_dist <- c()
-# for(i in xlab_val){
-#   if(as.numeric(tmp[i, "ko"]) > 0) {
-#     ko_dist <- c(ko_dist, rep.int(i,tmp[i, "ko"]))
-#   }
-# }
 
 dev.off()
